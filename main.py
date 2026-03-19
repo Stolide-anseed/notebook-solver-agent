98f7dc45d51e2dab6c63be27136612e5ba97f53b
Stolide-anseed <158155444+Stolide-anseed@users.noreply.github.com>
2026-03-19 19:36:28 +0300

    Merge remote-tracking branch 'origin/main'

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.preprocessing import preprocess_text
 from src.ingestion import extract_text_from_pdf
+from src.Text-splitter import splitter_text
 
 raw_text = extract_text_from_pdf("data/raw/3_BASIC_ML.pdf")
 clean_text = preprocess_text(raw_text)
---
+++ src/Text-splitter.py
@@ -0,0 +1,7 @@
+def splitter_text(text):
+    t_q_a = {
+    'theory':[],
+    'question':[],
+    'answer':[]
+    }
+    return text.splitlines()
